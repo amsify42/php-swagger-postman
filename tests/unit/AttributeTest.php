051e05627027af3ffe157bf1a710d4527427280d
Amsify42 <amsify42@gmail.com>
2024-07-24 15:26:32 +0530

    Create AttributeTest.php

---
+++ tests/unit/AttributeTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Amsify42\TestSP;
+
+use PHPUnit\Framework\TestCase;
+use Amsify42\PhpSwaggerPostman\Swagger;
+
+final class AttributeTest extends TestCase
+{
+	public function testSamples()
+	{
+		$this->assertTrue(true);
+	}
+}
